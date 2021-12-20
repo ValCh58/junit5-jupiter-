9f27e83d6323f448568c2333ce1712e68e8c9804
ValCh58 <chvaleriy@yahoo.com>
2021-12-20 14:11:07 +0500

    commit from 20-12-2021

--- src/main/java/com/example/project/Calculator.java
+++ src/main/java/com/example/project/Calculator.java
@@ -14,6 +14,16 @@ public class Calculator {
 	
 	static public void main(String[] args) {
 		System.out.println("Calculator TEST!!!!!!! CI/DI!!!!!!!!");
+		
+		//=========================================================
+		DeepThought ourSuperComputer = new DeepThought();
+        int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
+
+        RedisJava cache = new RedisJava();
+        cache.cache_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything",String.valueOf(ans));
+
+        System.out.println("Cached value: " + cache.get_cached_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything"));
+		//=========================================================
 	}
 
 	public int add(int a, int b) {
---
+++ src/test/java/com/example/project/TestRedisJava.java
@@ -0,0 +1,24 @@
+package com.example.project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestRedisJava {
+	public TestRedisJava(){
+
+    }
+
+    @Test
+    @DisplayName("Test Redis cache")
+    void testRedisCache(){
+        DeepThought ourSuperComputer = new DeepThought();
+        int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
+
+        RedisJava cache = new RedisJava();
+        cache.cache_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything",String.valueOf(ans));
+
+        assertEquals("42", cache.get_cached_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything"));
+    }
+}
